207dabe947b936b6c6c124852bba365b989a1c87
Zhou Hao <zhou.hao.27@gmail.com>
2017-04-11 22:54:03 +0800

    Support popup tooltip

---
+++ WOWMarkSlider/Classes/WOWTooltipView.swift
@@ -0,0 +1,21 @@
+//
+//  WOWTooltipView.swift
+//  Pods
+//
+//  Created by Zhou Hao on 11/4/17.
+//
+//
+
+import UIKit
+
+class WOWTooltipView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
